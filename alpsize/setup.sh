f5642252e762c1303815defe9f19f6c5b61ffe17
wistaria <wistaria@issp.u-tokyo.ac.jp>
2013-11-06 07:41:03 +0900

    Added more diffs

--- alpsize/setup.sh
+++ alpsize/setup.sh
@@ -5,6 +5,11 @@ test -z $SOURCE_DIR && SOURCE_DIR=$HOME/development/alps/tutorials
 set -x
 
 if test -d $SOURCE_DIR; then
-    cp -p $SOURCE_DIR/alpsize-02-original-c/wolff.c 02_wolff.c
-    (cd $SOURCE_DIR && diff -u alpsize-02-original-c/wolff.c alpsize-03-basic-cpp/wolff.C) > 03_wolff.diff
+    cp -p $SOURCE_DIR/alpsize-02-original-c/wolff.c 02-wolff.c
+    (cd $SOURCE_DIR && diff -u alpsize-02-original-c/wolff.c alpsize-03-basic-cpp/wolff.C) > 03-wolff.diff
+    (cd $SOURCE_DIR && diff -u alpsize-03-basic-cpp/wolff.C alpsize-04-stl/wolff.C) > 04-wolff.diff
+    (cd $SOURCE_DIR && diff -u alpsize-04-stl/wolff.C alpsize-05-boost/wolff.C) > 05-wolff.diff
+    (cd $SOURCE_DIR && diff -u alpsize-05-boost/wolff.C alpsize-06-parameters/wolff.C) > 06-wolff.diff
+    (cd $SOURCE_DIR && diff -u alpsize-06-parameters/wolff.C alpsize-07-alea/wolff.C) > 07-wolff.diff
+    (cd $SOURCE_DIR && diff -u alpsize-07-alea/wolff.C alpsize-08-lattice/wolff.C) > 08-wolff.diff
 fi
