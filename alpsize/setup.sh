8422685cee0e46b2886338908a4361a4b02e7735
wistaria <wistaria@issp.u-tokyo.ac.jp>
2013-11-06 09:30:46 +0900

    Updated alpsize-00-08, added 09 sample codes

--- alpsize/setup.sh
+++ alpsize/setup.sh
@@ -12,4 +12,8 @@ if test -d $SOURCE_DIR; then
     (cd $SOURCE_DIR && diff -u alpsize-05-boost/wolff.C alpsize-06-parameters/wolff.C) > 06-wolff.diff
     (cd $SOURCE_DIR && diff -u alpsize-06-parameters/wolff.C alpsize-07-alea/wolff.C) > 07-wolff.diff
     (cd $SOURCE_DIR && diff -u alpsize-07-alea/wolff.C alpsize-08-lattice/wolff.C) > 08-wolff.diff
+    cp -p $SOURCE_DIR/alpsize-09-scheduler/wolff_worker.h 09-wolff_worker.h
+    cp -p $SOURCE_DIR/alpsize-09-scheduler/wolff_worker.C 09-wolff_worker.C
+    cp -p $SOURCE_DIR/alpsize-09-scheduler/hello_worker.h 09-hello_worker.h
+    cp -p $SOURCE_DIR/alpsize-09-scheduler/hello_worker.C 09-hello_worker.C
 fi
