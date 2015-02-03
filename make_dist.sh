0132d2eae2a384f4da73a4b21fdecbe4d36715e8
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-02-03 09:59:47 +0900

    Changed language name jp->ja, etc

--- make_dist.sh
+++ make_dist.sh
@@ -12,18 +12,18 @@ else
   NOTEBOOK="alps-notebook-$DATE"
 fi
 
-make default
+make -j4 default
 
 DIR="$BASE"
-rm -rf $DIR $DIR.tgz $DIR.zip && mkdir -p $DIR/jp $DIR/en
+rm -rf $DIR $DIR.tgz $DIR.zip && mkdir -p $DIR/ja $DIR/en
 
-cp -fp overview/overview-normal.pdf $DIR/jp/01_overview.pdf
-cp -fp installation/installation-normal.pdf $DIR/jp/02_installation.pdf
-cp -fp tutorial/tutorial-normal.pdf $DIR/jp/03_tutorial.pdf
-cp -fp python/python-normal.pdf $DIR/jp/04_python.pdf
-cp -fp pyalps/pyalps-normal.pdf $DIR/jp/05_pyalps.pdf
-cp -fp matplotlib/matplotlib-normal.pdf $DIR/jp/06_matplotlib.pdf
-cp -fp alpsize/alpsize-normal.pdf $DIR/jp/07_alpsize.pdf
+cp -fp overview/overview-normal.pdf $DIR/ja/01_overview.pdf
+cp -fp installation/installation-normal.pdf $DIR/ja/02_installation.pdf
+cp -fp tutorial/tutorial-normal.pdf $DIR/ja/03_tutorial.pdf
+cp -fp python/python-normal.pdf $DIR/ja/04_python.pdf
+cp -fp pyalps/pyalps-normal.pdf $DIR/ja/05_pyalps.pdf
+cp -fp matplotlib/matplotlib-normal.pdf $DIR/ja/06_matplotlib.pdf
+cp -fp alpsize/alpsize-normal.pdf $DIR/ja/07_alpsize.pdf
 
 cp -fp overview/overview-en-normal.pdf $DIR/en/01_overview.pdf
 cp -fp tutorial/tutorial-en-normal.pdf $DIR/en/03_tutorial.pdf
@@ -32,29 +32,18 @@ tar zcf $DIR.tgz $DIR
 zip -r $DIR.zip $DIR
 
 DIR="$BASE-wide"
-rm -rf $DIR $DIR.tgz $DIR.zip && mkdir -p $DIR/jp $DIR/en
+rm -rf $DIR $DIR.tgz $DIR.zip && mkdir -p $DIR/ja $DIR/en
 
-cp -fp overview/overview-wide.pdf $DIR/jp/01_overview.pdf
-cp -fp installation/installation-wide.pdf $DIR/jp/02_installation.pdf
-cp -fp tutorial/tutorial-wide.pdf $DIR/jp/03_tutorial.pdf
-cp -fp python/python-wide.pdf $DIR/jp/04_python.pdf
-cp -fp pyalps/pyalps-wide.pdf $DIR/jp/05_pyalps.pdf
-cp -fp matplotlib/matplotlib-wide.pdf $DIR/jp/06_matplotlib.pdf
-cp -fp alpsize/alpsize-wide.pdf $DIR/jp/07_alpsize.pdf
+cp -fp overview/overview-wide.pdf $DIR/ja/01_overview.pdf
+cp -fp installation/installation-wide.pdf $DIR/ja/02_installation.pdf
+cp -fp tutorial/tutorial-wide.pdf $DIR/ja/03_tutorial.pdf
+cp -fp python/python-wide.pdf $DIR/ja/04_python.pdf
+cp -fp pyalps/pyalps-wide.pdf $DIR/ja/05_pyalps.pdf
+cp -fp matplotlib/matplotlib-wide.pdf $DIR/ja/06_matplotlib.pdf
+cp -fp alpsize/alpsize-wide.pdf $DIR/ja/07_alpsize.pdf
 
 cp -fp overview/overview-en-wide.pdf $DIR/en/01_overview.pdf
 cp -fp tutorial/tutorial-en-wide.pdf $DIR/en/03_tutorial.pdf
 
 tar zcf $DIR.tgz $DIR
 zip -r $DIR.zip $DIR
-
-# notebook
-if test -z "$DATE"; then
-  tar zcf $NOTEBOOK.tgz notebook
-  zip -r $NOTEBOOK.zip notebook
-else
-  rm -rf notebook-$DATE && mkdir -p notebook-$DATE
-  cp -rp notebook/* notebook-$DATE
-  tar zcf $NOTEBOOK.tgz notebook-$DATE
-  zip -r $NOTEBOOK.zip notebook-$DATE
-fi
